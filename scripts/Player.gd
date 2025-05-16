42a61c8d8cea79e333dabb4ff1b903b18dae798d
Shawn <shaochen.li@hotmail.com>
2025-05-16 14:55:50 +1000

    Arrows keys now move player
    Velocity is reset to 0 every frame unless a direction is pressed

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,16 +1,31 @@
 extends KinematicBody2D
 
+export var speed = 300
+var screen_size
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	velocity *= speed
+	position += velocity * delta
+	
+	# Prevent going off screen
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
