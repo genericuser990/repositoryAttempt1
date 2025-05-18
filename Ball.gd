aaa295629566a0ee0a5ce672707d4e6a4fddefe8
NotUserUserUserUserUser <noah.suzuki@education.nsw.gov.au>
2025-05-18 13:08:01 +1000

    gong game start
    win

---
+++ Ball.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var dx = 1;
+var dy = -1;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x += dx
+	position.y += dy
+
+
+
+
+func _on_RigidBody2D2_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print("bot")
+	dy *= -1
+
+
+
+
+func _on_RigidBody2D_body_entered(body):
+	print("top")
+	dy *= -1
