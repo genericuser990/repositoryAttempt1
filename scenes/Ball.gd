f6b87ab900520c755d363dcdc6d07f08636a6ca7
NotUserUserUserUserUser <noah.suzuki@education.nsw.gov.au>
2025-05-20 13:16:56 +1000

    changes to ball movement

--- scenes/Ball.gd
+++ scenes/Ball.gd
@@ -6,31 +6,16 @@ signal wall_bounced
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var speed = 100
+var vel = Vector2(speed, speed)
 
-var dx = 1;
-var dy = -1;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position.x += dx
-	position.y += dy
-
-func _on_Area_2D2_body_entered(body):
-	print("top")
-	dy *= -1
-	emit_signal("wall_bounced")
-
-
-func _on_Area_2D_body_entered(body):
-	print("bottom")
-	dy *= -1
-	emit_signal("wall_bounced")	
-
-func _on_Paddle_body_entered(body):
-	print("right")
-	dx *= -1
-	emit_signal("paddle_bounced")
+#called at set 60fps (more notes in doc)
+func _physics_process(delta):
+	var collsion = move_and_collide(vel* delta)
+	if collsion:
+		vel = vel.bounce(collsion.normal)
