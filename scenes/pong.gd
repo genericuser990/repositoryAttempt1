c9113fc27af784116b68dd0833b643fa7319385a
NotUserUserUserUserUser <noah.suzuki@education.nsw.gov.au>
2025-05-20 12:01:17 +1000

    connect signal

--- scenes/pong.gd
+++ scenes/pong.gd
@@ -6,16 +6,18 @@ extends Node2D
 # var b = "text"
 
 onready var ui := $Control
+onready var ball := $Ball
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ball.connect("paddle_bounced", self, "_on_Ball_paddle_bounced")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta):
 #	pass
-
 func _on_Ball_paddle_bounced():
 	ui.add1()
 
