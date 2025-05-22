558a9c61586e9ba85db843aca428c7df225693a8
NotUserUserUserUserUser <noah.suzuki@education.nsw.gov.au>
2025-05-22 15:50:45 +1000

    rope physics
    ropes go brrrr

---
+++ noahFolder/Drag.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+
+onready var button := $TextureButton
+var of := Vector2(0,0)
+var mouseDown = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	button.connect("button_down", self, "on_button_down")
+	button.connect("button_up", self, "on_button_up")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (mouseDown):
+		position = get_global_mouse_position() - of
+
+
+func on_button_down():
+	mouseDown = true
+	of = get_global_mouse_position() - position
+	
+func on_button_up():
+	mouseDown = false
---
+++ noahFolder/Rope.gd
@@ -0,0 +1,47 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var ropePeices := 5
+export var jointSoftness := 0.05
+var length
+
+onready var ropeEnd1 := $RopeEnd1
+onready var ropeEnd2 := $RopeEnd2
+export (PackedScene) var ropePeice
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	length = (ropeEnd2.position - ropeEnd1.position).length()
+	var ropePieceLength = length / ropePeices
+	print(ropePieceLength)
+	
+	var ratio =  1.0 / ropePeices
+	var angle = (ropeEnd2.position - ropeEnd1.position).angle() - PI/2
+	
+	var bef = ropeEnd1
+	
+	for i in ropePeices:
+		var peiceInstance = ropePeice.instance()
+		peiceInstance.position = ropeEnd1.position.linear_interpolate(ropeEnd2.position, ratio * (i+0.5))
+		peiceInstance.rotation_degrees = angle * 180 / PI
+		peiceInstance.setRopePiece(ropePieceLength)
+		add_child(peiceInstance)
+		
+		var joint = PinJoint2D.new()
+		joint.node_a = bef.get_path()
+		joint.node_b = peiceInstance.get_path()
+		joint.position = ropeEnd1.position.linear_interpolate(ropeEnd2.position, ratio * i)
+		joint.softness = jointSoftness
+		add_child(joint)
+		
+		bef = peiceInstance
+	
+	var joint = PinJoint2D.new()
+	joint.node_a = bef.get_path()
+	joint.node_b = ropeEnd2.get_path()
+	joint.position = ropeEnd2.position
+	add_child(joint)
+		
